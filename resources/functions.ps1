e6adbea51b64876ed3fc196116d0768bc9e34814
David Wiggs <david-wiggs@github.com>
2022-09-08 14:38:07 -0500

    Install into jenkins/.local/bin

--- resources/functions.ps1
+++ resources/functions.ps1
@@ -422,8 +422,8 @@ function New-MobSFScan {
 
     try {
         Write-Host "Installing mobsfscan dependencies"
-        Invoke-Expression 'export PATH="/var/lib/jenkins/.local/bin:$PATH"' # This could use cleanup
-        Invoke-Expression "python3-pip install mobsfscan"    
+        # Invoke-Expression 'export PATH="/var/lib/jenkins/.local/bin:$PATH"' # This could use cleanup
+        Invoke-Expression "python3-pip install mobsfscan --target /var/lib/jenkins/.local"    
     } catch {
         Write-Error "Unable to install required dependencies."
         Write-Error "$_"
