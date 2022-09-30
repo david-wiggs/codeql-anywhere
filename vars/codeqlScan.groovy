ebc24da6d45bb47b19e9fde95ea994a27ecd91c3
David Wiggs <david-wiggs@github.com>
2022-09-30 15:22:31 -0500

    Fix params typo

--- vars/codeqlScan.groovy
+++ vars/codeqlScan.groovy
@@ -1,24 +1,24 @@
 String call(Map params) {
     def scriptcontents = libraryResource "scripts/New-CodeQLScan.ps1"
     writeFile file: "New-CodeQLScan.ps1", text: scriptcontents
-    if (params.['codeQLDatabaseDirectoryPath']) {
-        codeQLDatabaseDirectoryPath = params.['codeQLDatabaseDirectoryPath']
+    if (params['codeQLDatabaseDirectoryPath']) {
+        codeQLDatabaseDirectoryPath = params['codeQLDatabaseDirectoryPath']
     } else {
         codeQLDatabaseDirectoryPath = 'codeql/databases'
     }
     
-    if (params.['pathToBuildScript']) {
-        pathToBuildScript = params.['pathToBuildScript']
+    if (params['pathToBuildScript']) {
+        pathToBuildScript = params['pathToBuildScript']
     } else {
         pathToBuildScript = ''
     }
     
-    if (params.['querySuite']) {
-        querySuite = params.['querySuite']
+    if (params['querySuite']) {
+        querySuite = params['querySuite']
     } else {
         querySuite = 'code-scanning'
     }
-    
+
     env.codeQLDatabaseDirectoryPath = codeQLDatabaseDirectoryPath
     env.pathToBuildScript = pathToBuildScript
     env.querySuite = querySuite
