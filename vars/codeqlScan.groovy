284cb954b3956ac3ee4e745996dccb822720467e
David Wiggs <david-wiggs@github.com>
2022-09-30 15:19:06 -0500

    Map params

--- vars/codeqlScan.groovy
+++ vars/codeqlScan.groovy
@@ -1,6 +1,24 @@
-String call(String codeQLDatabaseDirectoryPath = 'codeql/databases', String pathToBuildScript = '', String querySuite = 'code-scanning') {
+String call(Map params) {
     def scriptcontents = libraryResource "scripts/New-CodeQLScan.ps1"
     writeFile file: "New-CodeQLScan.ps1", text: scriptcontents
+    if (params.['codeQLDatabaseDirectoryPath']) {
+        codeQLDatabaseDirectoryPath = params.['codeQLDatabaseDirectoryPath']
+    } else {
+        codeQLDatabaseDirectoryPath = 'codeql/databases'
+    }
+    
+    if (params.['pathToBuildScript']) {
+        pathToBuildScript = params.['pathToBuildScript']
+    } else {
+        pathToBuildScript = ''
+    }
+    
+    if (params.['querySuite']) {
+        querySuite = params.['querySuite']
+    } else {
+        querySuite = 'code-scanning'
+    }
+    
     env.codeQLDatabaseDirectoryPath = codeQLDatabaseDirectoryPath
     env.pathToBuildScript = pathToBuildScript
     env.querySuite = querySuite
