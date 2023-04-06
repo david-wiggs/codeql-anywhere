72bbe73865a7b6dced1b7203190a7835bc008a7d
David Wiggs <107562400+david-wiggs@users.noreply.github.com>
2023-04-06 14:04:27 -0500

    Better support for Jenkins (#2)

--- vars/commitChecks.groovy
+++
@@ -1,5 +0,0 @@
-String call(Map params) {
-    def scriptcontents = libraryResource "scripts/Set-CommitStatusChecks.ps1"
-    writeFile file: "Set-CommitStatusChecks.ps1", text: scriptcontents
-    pwsh(returnStdout: true, script: "./Set-CommitStatusChecks.ps1 -token \$env:GITHUB_TOKEN")
-}
